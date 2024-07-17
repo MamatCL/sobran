4f186e66a59359eca9f4d68c0583c83bb706cddf
MamatCL <sobirinsujirin@gmail.com>
2024-07-17 22:45:37 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Family Laundry</title>
+</head>
+<body>
+<footer class="text-center" style="position: fixed; bottom: 0; width: 100%;">
+    <p>&copy; 2024 Family Laundry. Semua hak dilindungi.</p>
+</footer>
+</body>
+</html>
